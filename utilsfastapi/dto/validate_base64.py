6657daa382126870b88282a1418ca517a0adc391
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-06-30 20:42:16 +0330

    20240630

--- utilsfastapi/dto/validate_base64.py
+++ utilsfastapi/dto/validate_base64.py
@@ -2,19 +2,20 @@ from base64 import b64decode
 
 
 def validate_base64(value: str | list[str]) -> None:
-    if isinstance(value, str):
-        try:
-            new_value = value.split(",", 1)[1]
-            b64decode(new_value)
-        except Exception:
-            raise ValueError("فرمت base64 معتبر نیست.")
-    else:
-        for index, value_i in enumerate(value):
+    if value:
+        if isinstance(value, str):
             try:
-                new_value = value_i.split(",", 1)[1]
+                new_value = value.split(",", 1)[1]
                 b64decode(new_value)
-            except Exception as e:
-                error_message = f"فرمت base64 برای آیتم {index} معتبر نیست."
-                raise ValueError(error_message)
+            except Exception:
+                raise ValueError("فرمت base64 معتبر نیست.")
+        else:
+            for index, value_i in enumerate(value):
+                try:
+                    new_value = value_i.split(",", 1)[1]
+                    b64decode(new_value)
+                except Exception as e:
+                    error_message = f"فرمت base64 برای آیتم {index} معتبر نیست."
+                    raise ValueError(error_message)
 
     return None
