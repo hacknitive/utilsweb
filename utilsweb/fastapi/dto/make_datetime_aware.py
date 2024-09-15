2c562cf819475a53f55c3239b05a3473998593b8
Reza Aghamohammadi <re.agha.mo@gmail.com>
2024-09-15 21:07:25 +0330

    VERSION is updated to v0.1.6

--- utilsweb/fastapi/dto/make_datetime_aware.py
+++ utilsweb/fastapi/dto/make_datetime_aware.py
@@ -8,10 +8,11 @@ from typing import Tuple
 def make_datetime_aware(
         inputs: dict[str, datetime],
         datetime_field_names: Tuple[str, ...],
+        tzinfo=timezone.utc,
 ):
     for i in datetime_field_names:
         if inputs.get(i):
-            inputs[i] = inputs[i].replace(tzinfo=timezone.utc)
+            inputs[i] = inputs[i].replace(tzinfo=tzinfo)
             inputs[i] = inputs[i].replace(microsecond=0)
 
     return inputs
