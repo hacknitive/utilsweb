05bd6048bf4b21e68ebc52142cf94a44fccc37c0
Reza Aghamohammadi <re.agha.mo@gmail.com>
2024-09-16 15:20:21 +0330

    VERSION is updated to v0.1.7

--- utilsweb/fastapi/router/convert_module_name_to_route_name.py
+++ utilsweb/fastapi/router/convert_module_name_to_route_name.py
@@ -4,6 +4,6 @@ def convert_module_name_to_route_name(
         entity_name: str,
         module_name: str,
 ) -> str:
-    FILE_NAME = module_name.rstrip(".py").rsplit(sep, 1)[-1]
+    FILE_NAME = module_name[:-3].rsplit(sep, 1)[-1]
     return f"{entity_name}:{FILE_NAME}"
 
