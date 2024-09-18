cbaaddea21a613d0cdddde08d16047d86d542615
Reza Aghamohammadi <re.agha.mo@gmail.com>
2024-09-18 10:21:03 +0330

    VERSION is updated to v0.1.9

--- utilsweb/fastapi/middleware/cors.py
+++ utilsweb/fastapi/middleware/cors.py
@@ -3,12 +3,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
-def prepare_cors_middleware(app: FastAPI) -> None:
+def prepare_cors_middleware(
+        app: FastAPI,
+        allow_origins: list[str] = ["*"],
+        allow_credentials: bool = True,
+        allow_methods: list[str] = ["*"],
+        allow_headers: list[str] = ["*"],
+        expose_headers: list[str] = ["*"],
+) -> None:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-        expose_headers=["*"]
+        allow_origins=allow_origins,
+        allow_credentials=allow_credentials,
+        allow_methods=allow_methods,
+        allow_headers=allow_headers,
+        expose_headers=expose_headers,
     )
